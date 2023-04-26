ce4c2be4405c65f801fb0a13e1f7f0106684e757
Jess Cushnie <94955791+jmcushnie@users.noreply.github.com>
2023-04-27 09:42:32 +1200

    creates a new 2D array of Cell objects

--- ticTacToe/src/Board.java
+++ ticTacToe/src/Board.java
@@ -12,8 +12,8 @@ public class Board {
 	/** Constructor to create the game board */
 	public Board() {
 		
-	 //TODO: initialise the cells array using ROWS and COLS constants 
-
+	 //initialise the cells array using ROWS and COLS constants 
+		cells = new Cell[GameMain.ROWS][GameMain.COLS];
 		
 		for (int row = 0; row < GameMain.ROWS; ++row) {
 			for (int col = 0; col < GameMain.COLS; ++col) {
